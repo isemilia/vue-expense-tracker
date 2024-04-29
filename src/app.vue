22ef9b1726e20be0185ce0f0bd905b56de0030d9
Emilia <81705094+isemilia@users.noreply.github.com>
2024-04-29 11:03:33 +0500

    Set up basic structure

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,27 @@
 <template>
   <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
 </template>
 
 <script>
 import Header from '@/components/header.vue'
+import Balance from '@/components/balance.vue'
+import IncomeExpenses from '@/components/income-expenses.vue'
+import TransactionList from '@/components/transaction-list.vue'
+import AddTransaction from '@/components/add-transaction.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
   }
 }
 </script>
---
+++ src/components/balance.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>Your balance</h4>
+  <h2 id="balance">$0.00</h2>
+</template>
