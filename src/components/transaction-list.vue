fb49b76f0e5410303565a9491f6ae7e2b98a79ac
Emilia <81705094+isemilia@users.noreply.github.com>
2024-04-29 11:22:19 +0500

    Use data() & loop through the elements

--- src/components/transaction-list.vue
+++ src/components/transaction-list.vue
@@ -1,7 +1,27 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-    <li class="plus">Paycheck <span>$100</span><button class="delete-btn">x</button></li>
+    <li
+      v-for="transaction in transactions"
+      v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'"
+      v-bind:key="transaction.id"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'Billie Eilish', amount: -19.99 },
+        { id: 2, text: "Carla's Dreams", amount: 299.99 },
+        { id: 3, text: 'Ariana Grande', amount: -10 }
+      ]
+    }
+  }
+}
+</script>
