ce43e436267511e174a0d0f51e27f78ee5552422
Emilia <81705094+isemilia@users.noreply.github.com>
2024-04-29 15:16:26 +0500

    Calculate expense and income

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,8 @@
 <template>
   <Header />
   <div class="container">
-    <Balance v-bind:total="total.value" />
-    <IncomeExpenses />
+    <Balance v-bind:total="total" />
+    <IncomeExpenses v-bind:income="Math.abs(income)" v-bind:expense="Math.abs(expense)" />
     <TransactionList v-bind:transactions="transactions" />
     <AddTransaction />
   </div>
@@ -20,10 +20,29 @@ import { ref, computed } from 'vue'
 const transactions = ref([
   { id: 1, text: 'Billie Eilish', amount: -19.99 },
   { id: 2, text: "Carla's Dreams", amount: 299.99 },
-  { id: 3, text: 'Ariana Grande', amount: -10 }
+  { id: 3, text: 'Ariana Grande', amount: -10 },
+  { id: 4, text: 'Skillet', amount: 100 }
 ])
 
 const total = computed(() => {
   return transactions.value.reduce((acc, next) => acc + next.amount, 0)
+})
+
+const income = computed(() => {
+  return transactions.value.reduce((acc, next) => {
+    if (next.amount > 0) {
+      return acc + next.amount
+    }
+    return acc
+  }, 0)
+})
+
+const expense = computed(() => {
+  return transactions.value.reduce((acc, next) => {
+    if (next.amount < 0) {
+      return acc + next.amount
+    }
+    return acc
+  }, 0)
 })
 </script>
